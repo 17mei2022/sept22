16cdc7d62e58152e4941387610f3ba7190687432
Rajapolah <105721082+17mei2022@users.noreply.github.com>
2022-09-26 14:46:24 +0700

    Update px_2.sh

--- px_2.sh
+++ px_2.sh
@@ -13,7 +13,7 @@ cd
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 = 54.37.202.111:29879
+socks5 = 44.195.36.104:443
 socks5_username = mikrotik999
 socks5_password = Elibawnos
 END
